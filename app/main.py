ef80682d9aed4e92802b12948549163f779a0892
Ameer Amr <ameeralip@Ameer-ka-MacBook-Air.local>
2026-01-18 18:16:39 +0530

    removed create_all from main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from users.api.endpoints import auth
 from app.database import engine
 from users import models
 
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="PulseAPI", version="1.0.0")
 
